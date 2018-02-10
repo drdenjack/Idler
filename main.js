dd5c61bcded2eb07cff26833c2acd969ac095c27
Denny Jackson <dennis.jackson@perflogic.com>
2018-02-09 20:54:48 -0800

    use javascript to create table rows, add feet to the list of items

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
 var hands=111;
 var arms=1;
+var armCost=0;
+var feet=0;
+var footCost=0;
 
 function setElemData(id,val){
     document.getElementById(id).innerHTML = val;
@@ -9,6 +12,11 @@ function setElemData(id,val){
 function calcArmCost(){
     armCost = Math.floor(10 * Math.pow(1.1,arms));
     setElemData("armCost",armCost);
+}
+
+function calcFootCost(){
+    footCost = Math.floor(100 * Math.pow(1.1,feet));
+    setElemData("footCost",footCost);
 }
 
 function handClick(num){
@@ -26,10 +34,81 @@ function buyArm(){
     };
 };
 
+function buyFoot(){
+    if(hands >= footCost){                                   //checks that the player can afford the cursor
+        feet = feet + 1;                                   //increases number of cursors
+    	hands = hands - footCost;                          //removes the cookies spent
+	setElemData("feet",feet);
+	setElemData("hands",hands);
+	calcFootCost();
+    };
+};
+
+function makeTableRow(type,typeId,costId,costVal,clickFunc,clickArg){
+    var colWidth=100;
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    var name = document.createTextNode(type+": ");
+    td.appendChild(name);
+    td.width=colWidth;
+    tr.appendChild(td);
+    var span = document.createElement("span");
+    span.id=typeId;
+    span.innerHTML=type;
+    td.appendChild(span);
+    td.width=colWidth;
+    tr.appendChild(td);
+    var td = document.createElement("td");
+    if(costVal != null)
+    {
+    	var costText = document.createTextNode("Cost: ");
+    	td.appendChild(costText);
+    	var span = document.createElement("span");
+    	span.id=costId;
+    	span.innerHTML=costVal;
+    	td.appendChild(span);
+    }
+    td.width=colWidth;
+    tr.appendChild(td);
+    var td = document.createElement("td");
+    var button = document.createElement("Button");
+    button.innerHTML = "Buy "+type;
+    button.onclick = function(){clickFunc(clickArg)};
+    td.width=colWidth;
+    td.appendChild(button);
+    tr.appendChild(td);
+    
+    return tr;
+}
+
+function makeItemTable(){
+
+    var colWidth=100;
+    
+    var itemTableDiv = document.getElementById("itemTableDiv");
+    itemTableDiv.innerHTML="";
+    var table = document.createElement("TABLE");
+    var tableBody = document.createElement("TBODY");
+
+    tr = makeTableRow("Hands","hands",null,null,handClick,1);
+    tableBody.appendChild(tr);
+    tr = makeTableRow("Arms","arms","armCost",armCost,buyArm,1);
+    tableBody.appendChild(tr);
+    tr = makeTableRow("Feet","feet","footCost",footCost,buyFoot,1);
+    tableBody.appendChild(tr);
+	
+    table.appendChild(tableBody);
+    itemTableDiv.appendChild(table);
+    
+}
+
 function init(){
-    setElemData("arms",arms);
+    makeItemTable();
     setElemData("hands",hands);
+    setElemData("arms",arms);
+    setElemData("feet",feet);
     calcArmCost();
+    calcFootCost();
 }
 
 function saveStuff(){
@@ -37,6 +116,7 @@ function saveStuff(){
     var save = {
 	hands: hands,
 	arms: arms,
+	feet: feet,
     }
 
     localStorage.setItem("save",JSON.stringify(save));
@@ -45,9 +125,11 @@ function saveStuff(){
 function loadStuff(){
     var savegame = JSON.parse(localStorage.getItem("save"));
     if (typeof savegame.hands !== "undefined")
-	hands = savegame.hands;    
+	hands = savegame.hands;
     if (typeof savegame.arms !== "undefined")
-	arms = savegame.arms;
+    	arms = savegame.arms;
+    if (typeof savegame.feet !== "undefined")
+    	feet = savegame.feet;
     init();
 
     // // if successful ...
@@ -55,7 +137,7 @@ function loadStuff(){
 }
 
 window.setInterval(function(){
-    handClick(arms);
+    handClick(arms+10*feet);
     
 }, 1000);
 
